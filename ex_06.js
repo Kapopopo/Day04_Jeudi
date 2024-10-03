b1c0312d73bb6113eda93f6bc926b08934c457c3
kapopay <nawfal.yessoufou@epitech.eu>
2024-10-03 21:10:22 +0200

    finish

--- ex_06.js
+++ ex_06.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("resize", function () {
-    alert("la taille compte");
+    alert("Sizes matters");
   });
 
   let phone = document.querySelector('img[src="../Iphone22.png"]');
 
   if (phone) {
     phone.addEventListener("mouseover", function () {
-      alert("joli nn");
+	alert("Pretty, isn't it?");
     });
   }
 });
